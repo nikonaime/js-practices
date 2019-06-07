c24c7f982ae25de3f6fe2934a20a679f3f373b4f
Niko Nagervadze <nikonagervadze@gmail.com>
2019-06-07 12:14:31 +0100

    fix(10-Object-Oriented-Programming-Part-1): Finish task-1

---
+++ 10-Object-Oriented-Programming-Part-1/task-1.js
@@ -0,0 +1,31 @@
+function CoffeeMachine(power) {
+    
+	this.waterAmount = 0;
+	let WATER_HEAT_CAPACITY = 4200;
+	let self = this;
+    let timerId = 0;
+
+    function getBoilTime() {
+		return self.waterAmount * WATER_HEAT_CAPACITY * 80 / power;
+    }
+	
+    function onReady() {
+		console.log('Coffee is ready!');
+	}
+	
+    this.run = function() {
+		timerId = setTimeout(onReady, getBoilTime());
+    };
+
+    this.stop = function() {
+        clearTimeout(timerId);
+        console.log('Coffee is not ready!');
+        timerId = void 0;
+    }
+}
+
+
+let coffeeMachine = new CoffeeMachine(50000);
+coffeeMachine.waterAmount = 200;
+coffeeMachine.run();
+coffeeMachine.stop(); 
