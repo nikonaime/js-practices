5eb065d5a95f4f87a94dc96355164d984eddaf06
Niko Nagervadze <nikonagervadze@gmail.com>
2019-06-11 14:35:00 +0100

    feat(12-Introducing-JavaScript-Classes): Finish task-4

---
+++ 12-Introducing-JavaScript-Classes/task-4.js
@@ -0,0 +1,25 @@
+class MyString {
+  reverse(str) {
+    return str
+      .split("")
+      .reverse()
+      .join("");
+  }
+
+  ucFirst(str) {
+    return str[0].toUpperCase() + str.slice(1);
+  }
+
+  ucWords(str) {
+    return str
+      .split(" ")
+      .map(value => this.ucFirst(value))
+      .join(" ");
+  }
+}
+
+const str = new MyString();
+
+console.log(str.reverse("abcde")); // print 'edcba'
+console.log(str.ucFirst("abcde")); // print 'Abcde'
+console.log(str.ucWords("abcde abcde abcde")); // print 'Abcde Abcde Abcde'
