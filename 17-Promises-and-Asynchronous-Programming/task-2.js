0e8806117847aa483313e37ed880497605c7c936
Niko Nagervadze <nikonagervadze@gmail.com>
2019-06-14 20:27:07 +0400

    feat(17-Promises-and-Asynchronous-Programming): Finish task-2

---
+++ 17-Promises-and-Asynchronous-Programming/task-2.js
@@ -0,0 +1,53 @@
+const getCustomers = (customers, source) => {
+    const promise = new Promise((res, rej) => {
+        const result = customers
+            .map(customer => {
+                const match = source.find(country => country.id === customer.id);
+
+                if (!match) {
+                    rej(
+                        `We does not have information about country for this customer: ${
+                        customer.name
+                        }`
+                    );
+                }
+
+                return {
+                    ...customer,
+                    ...match
+                };
+            })
+            .filter(customer => customer.verified);
+
+        return res(result);
+    });
+
+    return promise;
+};
+
+const customers = [
+    {
+        id: "A1",
+        name: "Oliver",
+        verified: true
+    },
+    {
+        id: "A2",
+        name: "alex"
+    }
+];
+
+const countries = [
+    {
+        id: "A1",
+        country: "usa"
+    },
+    {
+        id: "A2",
+        country: "poland"
+    }
+];
+
+getCustomers(customers, countries)
+    .then(customers => console.log(customers))
+    .catch(error => console.log(error));
