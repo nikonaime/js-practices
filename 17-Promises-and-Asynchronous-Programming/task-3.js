3a04c481c0a55efeb650df6a2460f43f3a945f42
Niko Nagervadze <nikonagervadze@gmail.com>
2019-06-14 21:11:13 +0400

    feat(17-Promises-and-Asynchronous-Programming): Finish task-3

---
+++ 17-Promises-and-Asynchronous-Programming/task-3.js
@@ -0,0 +1,24 @@
+const get = require("fetch").fetchUrl;
+
+const url = "https://lab.lectrum.io/geo/api/countries?size=2";
+
+const send = () =>
+    new Promise((res, rej) => {
+        get(url, (error, meta, body) => {
+            if (meta.status !== 200) {
+                return rej(`We have error, status code: ${meta.status}`);
+            }
+            const {
+                data
+            } = JSON.parse(body);
+            return res(data);
+        });
+    });
+
+send(url)
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
