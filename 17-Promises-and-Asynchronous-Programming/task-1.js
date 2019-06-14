7174d30a7b462e5140f48c395cc3c901fb546498
Niko Nagervadze <nikonagervadze@gmail.com>
2019-06-14 20:20:19 +0400

    feat(17-Promises-and-Asynchronous-Programming): Finish task-1

---
+++ 17-Promises-and-Asynchronous-Programming/task-1.js
@@ -0,0 +1,23 @@
+const isCustomerVerified = data => {
+    const promise = new Promise((res, rej) => {
+        return data.verified ? res(true) : rej("Customer is not verified.");
+    });
+    return promise;
+};
+
+const personFirst = {
+    name: "Oliver",
+    verified: true
+};
+
+const personSecond = {
+    name: "Alex"
+};
+
+isCustomerVerified(personFirst)
+    .then(status => console.log(status))
+    .catch(error => console.log(error));
+
+isCustomerVerified(personSecond)
+    .then(status => console.log(status))
+    .catch(error => console.log(error)); 
